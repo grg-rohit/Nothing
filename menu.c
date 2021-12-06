83bb846585fc627e58059653d10ed5e8f1f1efeb
Rohit Gurung <grgrohit746@gmail.com>
2021-12-06 13:51:18 +0545

    hello boys how do you do

--- menu.c
+++ menu.c
@@ -5,8 +5,8 @@ void menu(){
     printf("\n\t\t\tMENU FOR BUS");
     printf("\n\n\n\t\t\t[1] View bus list\n\n");
     printf("\t\t\t[2] book Tickets\n\n");
-    printf("\t\t\t[3] Cacnel Booking\n\n");
-    printf("\t\t\t[4] Bus status\n\n");
+    printf("\t\t\t[3] Bus status\n\n");
+    printf("\t\t\t[4] Cancel Booking\n\n");
     printf("\t\t\t[5] Exit\n\n");
 }
 
